c18d90545a42cbd33adeb6a173e4038f5cb063fb
driedsoba <junleliw@gmail.com>
2026-03-04 15:21:10 +0800

    fix: alpine linux container image, backend s3 state

--- providers.tf
+++ providers.tf
@@ -1,12 +1,14 @@
 terraform {
+  backend "s3" {
+    bucket = "tfplayground-tf-state"
+    key    = "dev/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "6.34.0"
     }
   }
-}
-
-provider "aws" {
-  region = "ap-southeast-1"
 }
